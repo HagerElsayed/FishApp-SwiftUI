c51a29738b7a65af48aea69ef89ccb04a16c8a9b
Hager Elsayed <Hasanieenh@mobilenoc.mobi>
2022-05-25 20:18:27 +0200

    Initial Commit

---
+++ FishApp-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FishApp-SwiftUI
+//
+//  Created by Hager Elsayed on 25/05/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FishApp-SwiftUI/FishApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FishApp_SwiftUIApp.swift
+//  FishApp-SwiftUI
+//
+//  Created by Hager Elsayed on 25/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct FishApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
